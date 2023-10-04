c7fb96c8072f1ca8b531bec41ca8f59321ee8dd8
ILHAM RAHMAN <72718626+ilhamrahmannn@users.noreply.github.com>
2023-10-04 16:44:43 +0800

    new commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const video = document.querySelector('video');
+
+
+video.playbackRate = 0.548;
+
+
+
+
+window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.9;
+    audio.play();
+  });
